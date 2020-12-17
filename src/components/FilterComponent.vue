03ee89c03ac4c301dbb2e4d023d7a8c31307c9cc
Laporiettus <56964858+Laporiettus@users.noreply.github.com>
2020-12-17 14:22:39 +0300

    Revert "Revert "update 3""
    This reverts commit d40040acceed92473c869506ee57835f9633dabb.

---
+++ src/components/FilterComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-form v-model="formValidation">
+        <v-text-field
+                v-model="filterInfo"
+                label="Введите данные для поиска"
+        ></v-text-field>
+        <v-select
+                v-model="itemKey"
+                :items="optionsList"
+                label="Выберите тип фильтра"
+        ></v-select>
+        <v-btn
+                @click="applyFilter(itemKey, filterInfo)"
+        >
+            Применить фильтр
+        </v-btn>
+        <v-btn
+                @click="clearFilter()"
+                :disabled="!formValidation"
+        >
+            Сбросить фильтр
+        </v-btn>
+    </v-form>
+</template>
+
+<script>
+    export default {
+        name: "FilterComponent",
+        data() {
+            return {
+                itemKey: '',
+                filterInfo: '',
+                formValidation: false,
+                optionsList: ['Email', 'Телефон']
+            }
+        },
+        methods: {
+            applyFilter(key, val) {
+                this.$emit('on-apply-filter', {key, val})
+            },
+            clearFilter() {
+                this.$emit('on-clear-filter', '')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/headerMenu.vue
@@ -0,0 +1,33 @@
+<template>
+        <v-toolbar class="flex flex-row justify-items">
+            <v-btn @click="createUser">Создать пользователя</v-btn>
+            <v-btn @click="filterData">Фильтрация данных</v-btn>
+        </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: "headerMenu",
+        methods: {
+            createUser() {
+                this.$emit('on-toggle-new-user')
+            },
+            filterData() {
+                this.$emit('on-toggle-filter-data')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .justify-items {
+        justify-content: space-evenly;
+    }
+    /* div in header of v-toolbar */
+    ::v-deep .v-toolbar__content {
+        max-width: inherit;
+        width: 100%;
+        justify-content: space-evenly;
+    }
+
+</style>
